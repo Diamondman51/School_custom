31708f56f259f8413c747e0e94fc1a65b5be9cee
DIAMONDMAN <Diamondman51>
2025-02-18 00:47:47 +0500

    allowed hosts changed

--- core/settings.py
+++ core/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-pkhxw6#!y3aplwj061svug4m!cj!q!owj+1p37bywz8c6)=z58
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
